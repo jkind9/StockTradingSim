3956733ea66f00f075a84cee8d5a95a52613b368
jkind9 <jkind9@gmail.com>
2024-05-10 08:40:07 +0100

    altered url to get options as of specific times

--- polygon_test.py
+++ polygon_test.py
@@ -11,15 +11,21 @@ to_date = "2023-01-31"
 
 # URL for historical options data (example: Apple options)
 url = f"https://api.polygon.io/v3/reference/options/contracts?underlying_ticker="+ticker+"&as_of="+from_date+"&limit=520&apiKey=nfak2zW9PQJVjYSkqCS07Oc9dvEGFp9O"
-# url = "https://api.polygon.io/v2/aggs/ticker/O:SPY251219C00650000/range/1/day/2023-01-01/2023-01-31?adjusted=true&sort=asc&apiKey=nfak2zW9PQJVjYSkqCS07Oc9dvEGFp9O"
-# Make the request to Polygon.io
-response = requests.get(url)
-data = response.json()
-# print(data)
-# Extract and print specific fields like strike price, expiry date
-if 'results' in data:
-    for option in data['results']:
-        print(option)
-        break
-else:
-    print(f"No options data found for {ticker} within {from_date} to {to_date}.")
+
+for i in range(1, 31):
+    if i<10:
+        datevar = "0" + str(i)
+    else: datevar = str(i)
+    date = "2023-01-"+ datevar
+    print(date)
+    url = "https://api.polygon.io/v3/reference/options/contracts?underlying_ticker=AAPL&as_of="+date+"&limit=10&apiKey=nfak2zW9PQJVjYSkqCS07Oc9dvEGFp9O"
+    # Make the request to Polygon.io
+    response = requests.get(url)
+    data = response.json()
+    # print(data)
+    # Extract and print specific fields like strike price, expiry date
+    if 'results' in data:
+        for option in data['results']:
+            print(option)
+            break
+    
